45b88842d76b60e8f96dbc02edbdecbca23234a8
Aren-Step <aren1.step1.arm@gmail.com>
2025-05-01 16:13:08 +0400

    First commit from Visual Studio.

---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CSharpBasics
+{
+    class Program
+    {
+        public class A
+        {
+            public string Concat<T>(T item1, T item2)
+            {
+                string concat = item1.ToString() + item2.ToString();
+                return concat;
+            }
+        }
+
+        public struct Employee
+        {
+            public Employee() { }
+            public Employee(string name, int id) => (Name, Id) = (name, id);
+            public string? Name { get; set; }
+            public int Id { get; set; }
+            
+        }
+        // Driver Code
+        static void Main(string[] args)
+        {
+            #region Dictionary
+            //Dictionary<int, string> mydic = new Dictionary<int, string>()
+            //{
+            //    {1, "Davit Stepanyan" },
+            //    {2, "Oksana Kutikova" }
+            //};
+            //mydic.Add(123, "Aren Stepanyan");
+            //mydic.Add(124, "Arman Stepanyan");
+
+
+            //foreach (KeyValuePair<int, string> element1 in mydic)
+            //{
+            //    Console.WriteLine($"Person with ID {element1.Key} is {element1.Value}");
+            //}
+
+            #region finding key/value
+            //int i = Convert.ToInt32(Console.ReadLine());
+            //string n = Console.ReadLine();
+            //if (mydic.ContainsKey(i) == true)
+            //{
+            //    Console.WriteLine($"the ID {i} corresponds to the name {mydic.ElementAt(123)}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("there is no person with that ID");
+            //}
+            //if (mydic.ContainsValue(n) == true)
+            //{
+            //    Console.WriteLine($"there is a person named {n} that has an ID");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("there is no person with that name");
+            //}
+            #endregion
+
+            //Stack<Employee> stack = new Stack<Employee>();
+            //stack.Push(new Employee { Id = 1 });
+            //stack.Push(new Employee { Id = 2 });
+            //stack.Push(new Employee { Id = 3 });
+            //stack.Push(new Employee { Id = 4 });
+            //foreach (var element in stack)
+            //{
+            //    Console.WriteLine($"Stack Employee with ID {element.Id}");
+            //}
+
+            //Queue<Employee> queue = new Queue<Employee>();
+            //queue.Enqueue(new Employee { Id = 1 });
+            //queue.Enqueue(new Employee { Id = 2 }); 
+            //foreach (var element in queue)
+            //{
+            //    Console.WriteLine($"Queue Employee with ID {element.Id}");
+            //}
+
+            //LinkedList<Employee> list = new LinkedList<Employee>();
+            #endregion
+
+            //A obj = new A();
+            //obj.Concat<int>();
+
+
+            Console.WriteLine("Hello nice people!");
+            
+            
+            ////////////////////////////////////////////////////////////
+            // write a console app
+            // input numbers in dynamic list
+            // input the necessary number to find
+            // count steps for finding the specific number
+            ////////////////////////////////////////////////////////////
+
+        }
+    }
+}
---
+++ Exceptions/ExceptionBasics.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace ExceptionBasics
+{
+    public class NegativeArgumentException : ArithmeticException
+    {
+        public NegativeArgumentException(string? message) : base(message)
+        {
+
+        }
+    }
+    class ExceptionBasics
+    {
+        //static void swap<T>(ref T a, ref T b)
+        //{
+        //    T temp = a;
+        //    a = b;
+        //    b = temp;
+        //}
+        static int factorial(int n)
+        {
+            if (n < 0)
+                throw new NegativeArgumentException("Please Input an unsigned integer.");
+            if (n == 1 || n == 0)
+                return 1;
+            return n * factorial(n - 1);
+        }
+        //static int RemoveElement(int[] nums, int val)
+        //{
+        //    int k = nums.Length;
+        //    for (int i = 0; i < k; i++)
+        //    {
+        //        if (nums[i] == val)
+        //        {
+        //            swap(ref nums[i], ref nums[k - 1]);
+        //            k--;
+        //        }
+        //    }
+        //    return k;
+        //}
+        static void Main(string[] args)
+        {
+            {
+                //int[] nums = new int[7];
+                //for (int i = 0; i < nums.Length; i++)
+                //{
+                //    Console.Write($"Input number {i}: ");
+                //    nums[i] = Convert.ToInt32(Console.ReadLine());
+                //}
+                //Console.Write("Input number that needs to be removed: ");
+                //int val = Convert.ToInt32(Console.ReadLine());
+                //int k = RemoveElement(nums, val);
+                //Console.WriteLine("Array after removing " + val + ": ");
+                //for (int i = 0; i < nums.Length; i++)
+                //    Console.Write(nums[i] + (i == k - 1 ? "\n" : " "));
+                //Console.WriteLine("Number of elements after removing {0}: {1}", val, k);
+            }
+
+            int num1 = 6, num2 = 10;
+            try
+            {
+                Console.WriteLine($"{num1}! = {factorial(num1)}");
+                Console.WriteLine(num1 / num2);
+                throw new Exception("Exception from try block is thrown!");
+            }
+            catch (NegativeArgumentException)
+            {
+                Console.WriteLine("Negative argument!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Divided by zero!");
+            }
+            //finally
+            //{
+            //   //throw new Exception("Exception from finally block is thrown!");
+            //}
+        }
+    }
+}
