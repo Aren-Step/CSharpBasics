336e99136d9ab48f7a6f06c7248017cbcbc6d6d8
Aren <aren1.step1.arm@gmail.com>
2025-07-25 02:02:14 +0400

    Added simple functions in ThreadingBasics to check thread's state.

--- ThreadingBasics/Program.cs
+++ ThreadingBasics/Program.cs
@@ -3,32 +3,29 @@ namespace CSharpBasics;
 
 class ThreadingBasics
 {
-    static void Work(object? name)
+    static void PrintText()
     {
-        try 
-        {
-            Console.WriteLine($"Work from a thread {name}");
-
-            throw new Exception("An error occurred in the thread");
-
-        }
-        catch (Exception ex) {
-            Console.WriteLine($"Exception in thread {name}: {ex.Message}");
-        }
-        finally 
-        {
-            Console.WriteLine($"Thread {name} is finishing up.");
-        }
+        Thread.Sleep(3000);
+        Console.WriteLine("Hello from thread " + Thread.CurrentThread.ManagedThreadId);
     }
-    
+    static void CheckThreadState(Thread thread)
+    {
+        Console.WriteLine("The thread " + thread.Name + " is " + thread.ThreadState switch
+        {
+            ThreadState.WaitSleepJoin => "Blocked",
+            _ => thread.ThreadState
+        });
+    }
     static void Main()
     {
-        var t1 = new Thread(Work);
-        t1.Start("Thread 1");
-        Thread.Sleep(1000); // Wait for threads to finish
-        var t2 = new Thread(Work);
-        t2.Start("Thread 2");
-
-
+        Thread t1 = new Thread(PrintText);
+        t1.Name = "UltraThread69";
+        CheckThreadState(t1);
+        t1.Start();
+        CheckThreadState(t1);
+        Thread.Sleep(500);
+        CheckThreadState(t1);
+        t1.Join();
+        CheckThreadState(t1);
     }
 }
