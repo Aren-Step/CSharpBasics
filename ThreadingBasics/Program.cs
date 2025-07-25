2f828d1d5ee2e0c6a8bd26fc9f8f7063a271f151
Aren <aren1.step1.arm@gmail.com>
2025-07-25 15:23:25 +0400

    Synchronization. Using Monitor.Enter and Monitor.Exit

--- ThreadingBasics/Program.cs
+++ ThreadingBasics/Program.cs
@@ -3,29 +3,29 @@ namespace CSharpBasics;
 
 class ThreadingBasics
 {
-    static void PrintText()
+    public class ThreadSafe
     {
-        Thread.Sleep(3000);
-        Console.WriteLine("Hello from thread " + Thread.CurrentThread.ManagedThreadId);
-    }
-    static void CheckThreadState(Thread thread)
-    {
-        Console.WriteLine("The thread " + thread.Name + " is " + thread.ThreadState switch
+        static readonly object _locker = new object();
+        bool lockTaken = false;
+        static int _val1 = 1, _val2 = 1;
+
+        public static void Go()
         {
-            ThreadState.WaitSleepJoin => "Blocked",
-            _ => thread.ThreadState
-        });
+            ThreadSafe obj = new ThreadSafe();
+            Monitor.Enter(_locker, ref obj.lockTaken);
+            try
+            {
+                if (_val2 != 0) Console.WriteLine(_val1 / _val2);
+                _val2 = 0;
+                Console.WriteLine($"val1 = {_val1}, val2 = {_val2}");
+            }
+            finally {  if (obj.lockTaken) Monitor.Exit(_locker); }
+        }
     }
     static void Main()
     {
-        Thread t1 = new Thread(PrintText);
-        t1.Name = "UltraThread69";
-        CheckThreadState(t1);
+        Thread t1 = new(ThreadSafe.Go), t2 = new(ThreadSafe.Go);
         t1.Start();
-        CheckThreadState(t1);
-        Thread.Sleep(500);
-        CheckThreadState(t1);
-        t1.Join();
-        CheckThreadState(t1);
+        t2.Start();
     }
 }
