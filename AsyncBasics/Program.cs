402c0bc97769944c183116cf4471063bc5fb05a9
Aren <aren1.step1.arm@gmail.com>
2025-09-30 19:53:11 +0400

    Catching exceptions thrown in an async method called synchronously
    this is an example of aync method called synchorously by calling GetAwaiter().GetResult() or accessing Result property. The exception is wrapped in AggregateException when GetAwaiter().GetResult() is called.

--- AsyncBasics/Program.cs
+++ AsyncBasics/Program.cs
@@ -34,9 +34,41 @@ namespace AsyncBasics
             }
         }
 
+        static async Task<bool> AsyncMethodSyncCall(int id)
+        {
+            var result = Task.Run(() =>
+            {
+                if (id < 0)
+                {
+                    throw new InvalidOperationException("Exception thrown from Task.Run()");
+                }
+                return false;
+            }).Result;
+            // If GetAwaiter().GetResult() is called instead of Result, the exception won't be wrapped in a AggregateException exception.
+            
+            if (id == 0)
+            {
+                result = false;
+                throw new InvalidOperationException("Exception thrown from AsyncMethodSyncCall()");
+            }
+            return result;
+        }
+
         public static async Task Main()
         {
-            TryUpdateDataAsync(100);
+            Task<bool> myTask;
+            try
+            {
+                myTask = AsyncMethodSyncCall(-100);
+                Console.WriteLine("The task returned" + await myTask);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"The exception of type {ex.GetType()} was catched!" + 
+                    "\nThe message from the exception:\n" + ex.Message);
+            }
+
+            //TryUpdateDataAsync(100);
         }
     }
 }
