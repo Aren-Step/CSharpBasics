eda284614bf72e12b819a405330bf79421fc45af
Aren <aren1.step1.arm@gmail.com>
2025-10-01 16:59:02 +0400

    Parallelism in async methods
    Demonstrates two iterating async methods that work concurrently after the await statement is complete.

--- AsyncBasics/Program.cs
+++ AsyncBasics/Program.cs
@@ -4,71 +4,37 @@ namespace AsyncBasics
 {
     class Program
     {
-        class BackgroundTask
+        public static async Task IterateAsync()
         {
-            public static async Task UpdateDataAsync(int id)
+            for (int i = 1; i <= 50; i++)
             {
-                await Task.Delay(5000);
-            }
-        }
+                Console.WriteLine($"{i} \t Async method called \t" + 
+                    $"Executing threadID: {Thread.CurrentThread.ManagedThreadId}, Is it ThreadPoolThread? {Thread.CurrentThread.IsThreadPoolThread}");
+                if (i == 10) 
+                    await Task.Delay(1).ConfigureAwait(true);  // ConfigureAwait(true) does not change the await statement's default behavior.
+                                                               // it's called to clearly show that the execution continues on the captured context.
 
-        static async Task TryUpdateDataAsync(int id)
-        {
-            try
-            {
-                await BackgroundTask.UpdateDataAsync(id);
+                /*
+                 * When the async method completes its awaited task, the control from the calling method (in this case - Main()) returns to itself
+                 * and the async method grabs a thread from the ThreadPool, but the calling method continues executing, creating parallelism.
+                 * The end result in this case is UNDETERMINISTIC.
+                 * NOTE: Whether the captured context after the await statement's completion changes or not, the executing thread MAY CHANGE.
+                 */
             }
-            catch (Exception ex) 
-            { 
-                if (ex is AggregateException taskEx)
-                {
-                    foreach (var innerEx in taskEx.InnerExceptions)
-                    {
-                        Console.WriteLine(innerEx.Message);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-        }
-
-        static async Task<bool> AsyncMethodSyncCall(int id)
-        {
-            var result = Task.Run(() =>
-            {
-                if (id < 0)
-                {
-                    throw new InvalidOperationException("Exception thrown from Task.Run()");
-                }
-                return false;
-            }).Result;
-            // If GetAwaiter().GetResult() is called instead of Result, the exception won't be wrapped in a AggregateException exception.
-            
-            if (id == 0)
-            {
-                result = false;
-                throw new InvalidOperationException("Exception thrown from AsyncMethodSyncCall()");
-            }
-            return result;
         }
 
         public static async Task Main()
         {
-            Task<bool> myTask;
-            try
-            {
-                myTask = AsyncMethodSyncCall(-100);
-                Console.WriteLine("The task returned" + await myTask);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"The exception of type {ex.GetType()} was catched!" + 
-                    "\nThe message from the exception:\n" + ex.Message);
-            }
+            Task iterateTask = IterateAsync();
 
-            //TryUpdateDataAsync(100);
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine($"{i} \t Calling method called \t" + 
+                    $"Executing threadID: {Thread.CurrentThread.ManagedThreadId}, Is it ThreadPoolThread? {Thread.CurrentThread.IsThreadPoolThread}");
+                if (i == 10)
+                    await Task.Delay(1);   // Parallelism is caused after the completion of the IterateAsync()'s await statement.
+                                           // It's enough to await once to create concurrency.
+            }
         }
     }
 }
